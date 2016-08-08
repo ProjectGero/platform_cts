bluestacks/cts-a16

DO NOT MERGE: Cts: Add checks for testing CREATE_USERS permission.
Bug: 29189712
Change-Id: Icff148ee55e8339cbde030b547788d424509b9a3

hostsidetests/multiuser/src/android/host/multiuser/CreateUsersPermissionTest.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package android.host.multiuser;
+
+public class CreateUsersPermissionTest extends BaseMultiUserTest {
+    public void testCantSetUserRestriction() throws Exception {
+        final String setRestriction = "pm set-user-restriction no_unmute_microphone ";
+        final String output = getDevice().executeShellCommand(setRestriction + "1");
+        assertTrue("Trying to set user restriction should fail with SecurityException",
+                output.startsWith("Error") && output.contains("SecurityException"));
+    }
+}

ProjectGero/platform_cts

hostsidetests/multiuser/src/android/host/multiuser/CreateUsersPermissionTest.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package android.host.multiuser;
+
+public class CreateUsersPermissionTest extends BaseMultiUserTest {
+    public void testCantSetUserRestriction() throws Exception {
+        final String setRestriction = "pm set-user-restriction no_fun ";
+        final String output = getDevice().executeShellCommand(setRestriction + "1");
+        assertTrue("Trying to set user restriction should fail with SecurityException",
+                output.startsWith("Error") && output.contains("SecurityException"));
+    }
+}